5205d71094a05e15c6bf8969e10fd88340805a75
Anh Lê Việt <anhleviet@anhs-mac.local>
2016-09-29 21:07:22 +0700

    commit

--- Demo4/ViewController.swift
+++ Demo4/ViewController.swift
@@ -10,7 +10,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+    
+    
+    @IBOutlet weak var txtUsername: UITextField!
+    
+    @IBOutlet weak var tvListUser: UITextView!
+    
+    @IBOutlet weak var txtEmail: UITextField!
+    
+    @IBOutlet weak var txtPass: UITextField!
+    
+    var alertView : UIAlertView = UIAlertView()
+    var listUser : String  = "List User:"
+    var i : Int = 1
+    @IBOutlet weak var txtUserProfile: UITextView!
+    @IBAction func btnClear(_ sender: AnyObject) {
+        txtUsername.text = ""
+        txtPass.text = ""
+        txtEmail.text = ""
+    }
+    
+    
+    
+    @IBAction func btnReg(_ sender: AnyObject) {
+            if (txtUsername.text?.isEmpty)! || (txtEmail.text?.isEmpty)! || (txtPass.text?.isEmpty)! {
+                alertView.title = "error"
+                alertView.message = "Please fill all the feilds!"
+                alertView.addButton(withTitle: "OK")
+                alertView.show()
+            }
+            else {
+                alertView.title = "Success"
+                alertView.message = "Register Successfully"
+                alertView.addButton(withTitle: "OK")
+                alertView.show()
+                listUser += "\n" + String(i) +  "." +   txtUsername.text!
+                tvListUser.text = listUser
+                i += 1
+            }
+        
+    }
+    
+       override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
